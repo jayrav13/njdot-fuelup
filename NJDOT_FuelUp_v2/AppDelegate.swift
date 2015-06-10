332813a978c3a59b146ff34d1a9ac5d2e88f2313
Jay Ravaliya <jayrav13@gmail.com>
2015-06-10 17:10:29 -0400

    Added Parse Framework

--- NJDOT_FuelUp_v2/AppDelegate.swift
+++ NJDOT_FuelUp_v2/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("FomuzZruHIHwe3GdacUqlmc2P5NmnUuEBlevX1mF",
+            clientKey: "hHakwRoo1d384eNBrlr678IgfBZerohrNnNuZfpd")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
