ec7e5652ec60449b09ae4d625313f0d88dd1162a
Jay Ravaliya <jayrav13@gmail.com>
2015-07-19 00:52:02 -0400

    Functioning JRDetailsView Class

--- NJDOT_FuelUp_v2/BridgesTableViewController.swift
+++ NJDOT_FuelUp_v2/BridgesTableViewController.swift
@@ -8,98 +8,82 @@
 
 import UIKit
 
+// global variables used by other views
 var searchResults : NSMutableArray!
 var isBlank : Bool = true
 
 class BridgesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate {
 
-    var searchBar : UISearchBar!
+    // set up search bar, tableView
     var tableView : UITableView!
     let searchController : UISearchController = UISearchController(searchResultsController: nil)
     
+    // add variables for height/width
     var height : CGFloat = UIScreen.mainScreen().bounds.height
     var width : CGFloat = UIScreen.mainScreen().bounds.width
     
+    // bar button items
     var backButton : UIBarButtonItem!
-    var anotherButton : UIBarButtonItem!
+    
+    // sets up DetailsView View and opaqueView
+    var opaqueView : UIView!
+    var jr : JRDetailsView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // set background color of viewcontroller to white and set navItem title
         view.backgroundColor = UIColor.whiteColor()
         self.navigationItem.title = "Search Bridge"
         
+        // add to bar button items
         backButton = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: self, action: "backButton:")
         self.navigationItem.leftBarButtonItem = backButton
         
-        anotherButton = UIBarButtonItem(title: "Test", style: UIBarButtonItemStyle.Plain, target: self, action: "anotherButtonTest:")
-        self.navigationItem.rightBarButtonItem = anotherButton
-        
+        // set up tableView
         tableView = UITableView(frame: view.frame)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
         
+        // set up searchController
         searchController.searchResultsUpdater = self
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.dimsBackgroundDuringPresentation = false
         searchController.searchBar.sizeToFit()
+        searchController.searchBar.delegate = self
         self.tableView.tableHeaderView = searchController.searchBar
         
+        // set up searchResults array to have nothing
         searchResults = []
         
-        searchController.searchBar.delegate = self
-        
+        // when loaded, searchController should be blank with no text
         isBlank = true
         searchController.searchBar.text = ""
+        
+        jr = JRDetailsView(frame: view.frame)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    var jr : JRDetailsView = JRDetailsView(frame: CGRectMake(UIScreen.mainScreen().bounds.width/2-100, 100, 200, 300))
-    
-    func anotherButtonTest(sender: UIButton!)
-    {
-        /*let alert: UIAlertController = UIAlertController(title: "Loading...", message: "0% Complete\nTest", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Test", style: UIAlertActionStyle.Cancel, handler: { (action : UIAlertAction!) -> Void in
-            
-        }))
-        
-        self.presentViewController(alert, animated: true) { () -> Void in
-            println("Worked")
-        }*/
-        
-
-        if jr.window != nil
-        {
-            jr.removeFromSuperview()
-            tableView.alpha = 1
-            tableView.userInteractionEnabled = true
-        }
-        else
-        {
-            view.addSubview(jr)
-            tableView.alpha = 0.25
-            jr.alpha = 1
-            tableView.userInteractionEnabled = false
-        }
-        
-    }
-    
+    // when row is selected
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        searchController.dismissViewControllerAnimated(true, completion: { () -> Void in
-            
-        })
-        
-        let alertController = UIAlertController(title: "Details", message: "Message", preferredStyle: UIAlertControllerStyle.ActionSheet)
-        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action: UIAlertAction!) -> Void in
-            
+        // if the user is typing something, dismiss this viewcontroller first
+        if searchController.view.window != nil
+        {
+            searchController.dismissViewControllerAnimated(true, completion: { () -> Void in
+                
+            })
         }
         
+        // variable for object associated with row selected
         var currentObject : NSMutableArray! = []
+        
+        // if the search is blank, the tableView will be displaying data from the bridgesArray
+        // else, it will be displaying data from the searchResults array
         if(isBlank == true)
         {
             currentObject.addObject(bridgesArray[indexPath.row])
@@ -109,6 +93,7 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
             currentObject.addObject(searchResults[indexPath.row])
         }
         
+        // new button for users to navigate to bridge
         let navigate = UIAlertAction(title: "Navigate", style: UIAlertActionStyle.Default) { (action: UIAlertAction!) -> Void in
 
             var latitude : Double = currentObject[0]["latitude"] as! Double
@@ -124,21 +109,32 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
             }
         }
         
-        alertController.addAction(cancel)
-        alertController.addAction(navigate)
+        jr.object = currentObject
+        // present the view and deselect the row directly after
         
-        self.presentViewController(alertController, animated: true) { () -> Void in
-
+        UIView.transitionWithView(self.view, duration: 0.25, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+            
+                self.view.addSubview(self.jr)
+            
+            }) { (finished: Bool) -> Void in
+                
         }
         
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
+    // determine how many rows are in the main section
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        // if the search box is blank, the number of rows will be the total number of 
+        // structures
         if(isBlank == true)
         {
             return bridgesArray.count
         }
+        // otherwise, check how many objects are in searchResults
+        // if none, just return 1
+        // else, return the number of rows in searchResults
         else
         {
             if(searchResults.count == 0)
@@ -152,15 +148,22 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
         }
     }
     
+    // for each cell in the tableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // dequeue cell, establish cell using the subtitle style, align everything left to start
         var cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
         cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
         cell.textLabel?.textAlignment = NSTextAlignment.Left
         
+        // if the search is blank
         if(isBlank == true)
         {
+            // set up the primary text using the Structure No
             cell.textLabel?.text = "Structure No: " + (bridgesArray[indexPath.row]["str_no"] as! String)
             
+            // safely unwrap the street name and municipality
+            // if both exists, add them to the tableView
+            // else, show that it is unavailable
             if let str_name : String = bridgesArray[indexPath.row]["str_name"] as? String
             {
                 if let municipality : String = bridgesArray[indexPath.row]["municipality"] as? String
@@ -179,14 +182,18 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
             
             
         }
+        // if the user is currently searching
         else
         {
+            // if there is no matching result, use a default cell center-aligned and notify
+            // user that there is no result found
             if(searchResults.count == 0)
             {
                 cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
                 cell.textLabel?.textAlignment = NSTextAlignment.Center
                 cell.textLabel?.text = "No results found."
             }
+            // otherwise, do what we did earlier and list out Structure No and str_name/municipality if they exist
             else
             {
                 cell.textLabel?.text = "Structure No: " + (searchResults[indexPath.row]["str_no"] as! String)
@@ -207,16 +214,25 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
                 }
             }
         }
-        
+    
+        // return the cell
         return cell
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    // each time the user changes the search box entry, update the searchResults array and refresh tableView
     func updateSearchResultsForSearchController(searchController: UISearchController) {
-        // println(searchController.searchBar.text)
+        // if there is text in the searchBar
         if count(searchController.searchBar.text) != 0
         {
+            // clean searchResults query, change bool value and get ready for an O(n) search on bridgesArray to find matches
             searchResults = []
             isBlank = false
+            
+            // search all objects and add matching objects (non-case-sensitive) to searchResults
             for var i = 0; i < bridgesArray.count; i++
             {
                 if (((bridgesArray[i]["str_no"] as! String).uppercaseString).rangeOfString((searchController.searchBar.text).uppercaseString) != nil)
@@ -225,20 +241,23 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
                 }
             }
         }
+        // if blank, just change boolean - the output will be handled by the tableView delegate methods
         else
         {
             isBlank = true
         }
 
+        // reload all data in tableView
         tableView.reloadData()
     }
     
+    // dismiss viewcontroller after making sure that searchController has been dismissed
     func backButton(sender: UIButton!)
     {
         if searchController.view.window != nil
         {
             searchController.dismissViewControllerAnimated(true, completion: { () -> Void in
-                
+
             })
         }
         self.dismissViewControllerAnimated(true, completion: { () -> Void in
@@ -246,6 +265,7 @@ class BridgesTableViewController: UIViewController, UITableViewDataSource, UITab
         })
     }
     
+    // clear text when cancel button is clicked
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
         isBlank = true
         tableView.reloadData()
--- NJDOT_FuelUp_v2/JRDetailsView.swift
+++ NJDOT_FuelUp_v2/JRDetailsView.swift
@@ -8,21 +8,11 @@
 
 import UIKit
 
-class JRDetailsView: UIView {
+public class JRDetailsView: UIView {
 
-    var county : String!
-    var latitude : Double!
-    var longitude : Double!
-    var mp : Double!
-    var municipality : String!
-    var owner : String!
-    var route : String!
-    var str_name : String!
-    var str_no : String!
-    
-    func setTableViewAlpha(view: BridgesTableViewController) {
-        view.tableView.alpha = 1
-    }
+    // variables
+    var opaqueView : UIView!
+    var object: NSMutableArray!
     
     convenience init()
     {
@@ -35,33 +25,60 @@ class JRDetailsView: UIView {
         self.createCustomView()
     }
     
-    required init(coder aDecoder: NSCoder)
+    required public init(coder aDecoder: NSCoder)
     {
         fatalError("init(coder:) has not been implemented")
     }
     
-    internal func setValues(county : String, latitude: Double, longitude: Double, mp: Double, municipality: Double, owner: String, route: String, str_name: String, str_no: String)
+    public func createCustomView()
     {
+        var height: CGFloat = CGFloat(UIScreen.mainScreen().bounds.height)
+        var width: CGFloat = CGFloat(UIScreen.mainScreen().bounds.width)
         
-    }
-    
-    func createCustomView()
-    {
-        var label : UILabel = UILabel(frame: CGRectMake(0, 0, 200, 200))
-        label.backgroundColor = UIColor.blackColor()
+        opaqueView = UIView(frame: self.frame)
+        opaqueView.backgroundColor = UIColor.blackColor()
+        opaqueView.alpha = 0.5
+        self.addSubview(opaqueView)
+        
+        var label : UILabel = UILabel(frame: CGRectMake(width/2.0 - 150, 100, 300.0, height - 200.0))
+        label.backgroundColor = UIColor.whiteColor()
         label.text = "Test"
-        label.textColor = UIColor.whiteColor()
+        label.textColor = UIColor.blackColor()
         self.addSubview(label)
         
-        var button : UIButton = UIButton(frame: CGRectMake(0, 0, 200, 200))
-        button.setTitle("Button", forState: UIControlState.Normal)
+        var button : UIButton = UIButton(frame: CGRectMake(width/2-50, height/2-50, 100, 50))
+        
+        if object.count > 0
+        {
+            if let test: String = object[0]["municipality"] as? String
+            {
+                button.setTitle(test, forState: UIControlState.Normal)
+            }
+            else
+            {
+                button.setTitle("Error", forState: UIControlState.Normal)
+            }
+        }
+        else
+        {
+            button.setTitle("Error", forState: UIControlState.Normal)
+        }
+
         button.addTarget(self, action: "buttonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
         self.addSubview(button)
+        
     }
     
     func buttonPressed(sender: UIButton!)
     {
-        self.removeFromSuperview()
+        UIView.transitionWithView(self.superview!, duration: 0.25, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+            
+                self.removeFromSuperview()
+            
+            }) { (finished: Bool) -> Void in
+
+        }
     }
     
     
